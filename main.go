2085f938484ef5e5886e40a0a17c0a1e2463635f
sisimogangg <godfrey@sisimogang.co.za>
2019-07-05 12:33:30 +0200

    Emitting cancellation event

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
@@ -8,16 +10,56 @@ import (
 )
 
 func main() {
-	http.ListenAndServe(":8000", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
+	testCancelEmit := false
+	if testCancelEmit {
+		http.ListenAndServe(":8000", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
 
-		fmt.Fprint(os.Stdout, "processing request\n")
+			fmt.Fprint(os.Stdout, "processing request\n")
 
-		select {
-		case <-time.After(2 * time.Second):
-			w.Write([]byte("request processed"))
-		case <-ctx.Done():
-			fmt.Fprint(os.Stderr, "request cancelled\n")
+			select {
+			case <-time.After(2 * time.Second):
+				w.Write([]byte("request processed"))
+			case <-ctx.Done():
+				fmt.Fprint(os.Stderr, "request cancelled\n")
+			}
+		}))
+	} else {
+		runOperations()
+	}
+}
+
+func operation1(ctx context.Context) error {
+	time.Sleep(100 * time.Millisecond)
+	return errors.New("failed")
+}
+
+func operation2(ctx context.Context) {
+	select {
+	case <-time.After(500 * time.Millisecond):
+		fmt.Println("done")
+	case <-ctx.Done():
+		fmt.Println("halted operation2")
+	}
+}
+
+func runOperations() {
+	// Create a new context
+	ctx := context.Background()
+	// Create a new context, with its cancellation function
+	// from the original context
+	ctx, cancel := context.WithCancel(ctx)
+
+	// Run two operations: one in a different go routine
+	go func() {
+		err := operation1(ctx)
+		// If this operation returns an error
+		// cancel all operations using this context
+		if err != nil {
+			cancel()
 		}
-	}))
+	}()
+
+	// Run operation2 with the same context we use for operation1
+	operation2(ctx)
 }
