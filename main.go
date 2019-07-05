53fa2cf34303de7df90d41c0738fd4cdfb889ea1
sisimogangg <godfrey@sisimogang.co.za>
2019-07-05 12:21:50 +0200

    Listening to cancellation event

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	http.ListenAndServe(":8000", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		fmt.Fprint(os.Stdout, "processing request\n")
+
+		select {
+		case <-time.After(2 * time.Second):
+			w.Write([]byte("request processed"))
+		case <-ctx.Done():
+			fmt.Fprint(os.Stderr, "request cancelled\n")
+		}
+	}))
+}
